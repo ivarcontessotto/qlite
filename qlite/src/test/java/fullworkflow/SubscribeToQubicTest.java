09cf8cfa6a54c5c366a6f9fb2a768ce43b561883
Fabian Brand HSLU <fabian.brand@stud.hslu.ch>
2019-11-10 17:05:45 +0100

    subscribe to qubic test

--- qlite/src/test/java/fullworkflow/SubscribeToQubicTest.java
+++ qlite/src/test/java/fullworkflow/SubscribeToQubicTest.java
@@ -1,9 +1,18 @@
-package qubic;
+package fullworkflow;
 
+import oracle.OracleManager;
+import oracle.OracleReader;
+import oracle.OracleWriter;
+import org.json.JSONObject;
 import org.junit.Test;
+import qubic.EditableQubicSpecification;
+import qubic.QubicReader;
+import qubic.QubicSpecification;
+import qubic.QubicWriter;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class SubscribeToQubicTest {
 
@@ -22,6 +31,31 @@ public class SubscribeToQubicTest {
         eqs.setCode(code);
 
         qubicWriter.publishQubicTransaction();
+        String qubicTransactionHash = qubicWriter.getQubicTransactionHash();
+        String qubicId = qubicWriter.getID();
+        QubicReader qr = new QubicReader(qubicId);
+        OracleWriter oracleWriter = new OracleWriter(qr);
+
+
+        OracleManager om = new OracleManager(oracleWriter);
+        om.start();
+
+        do{
+            List<JSONObject> possibleSubscribers = qubicWriter.fetchApplications();
+        } while (qubicWriter.fetchApplications().isEmpty());
+
+
+//        qubicWriter.getAssembly().add(oracleWriter.getID());
+//        qubicWriter.publishAssemblyTransaction();
+//        oracleWriter.doHashStatement(position);
+//        oracleWriter.doResultStatement();
+//        OracleReader oracleReader = new OracleReader(oracleWriter.getID());
+//        oracleReader.getHashStatementReader().read(position);
+//        oracleReader.getResultStatementReader().read(position);
+
+
+
+
         String assertMessage = "qubic transaction hash: " + qubicWriter.getQubicTransactionHash();
         QubicReader qubicReader = new QubicReader(qubicWriter.getID());
         QubicSpecification read_qs = qubicReader.getSpecification();
