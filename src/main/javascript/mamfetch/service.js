6ea995ee13df50425ce357da0abf5d2bc19f067a
Ivar Contessotto <ivar.contessotto@stud.hslu.ch>
2019-11-23 14:30:17 +0100

    working version of mam read service

--- src/main/javascript/mamfetch/service.js
+++ src/main/javascript/mamfetch/service.js
@@ -9,31 +9,54 @@ if (!process.argv[3]) return console.log('Missing Argument: An URL for a public 
 const provider = process.argv[3];
 
 if (!process.argv[4]) return console.log('Missing Argument: The root address of the MAM stream to listen to is required!');
-const root = process.argv[4];
+let nextRoot = process.argv[4];
 
 const mode = 'public';
 const key = null;
 const localhost = '127.0.0.1';
 
+const pollingIntervallMilliseconds = 5 * 1000;
 let latestMessage = null;	
-
-function convertToJsonString(message) {
-	return JSON.stringify(JSON.parse(Converter.trytesToAscii(message)));
-}
-
-function fetchLatestMessage() {
-	const response = Mam.fetch(root, mode, key);
-	response.then(resolve => {
-		latestMessage = convertToJsonString(resolve.messages[resolve.messages.length - 1]);
-	});
-}
 
 function sleep(milliseconds) {
    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-async function publishLatestMessage(client) {
+function convertToJson(message) {
+	return JSON.parse(Converter.trytesToAscii(message));
+}
+
+function fetchLatestMessage(root) {
+	console.log(`Fetch latest message from root ${root}`);
+	const response = Mam.fetch(root, mode, key);
+	
+	response.then(resolve => {
+		console.log('Resolve response');
+		nextRoot = resolve.nextRoot;
+		latestMessage = convertToJson(resolve.messages[resolve.messages.length - 1]);
+	}).catch(error => { 
+		console.log(`Fetch error: ${error}`);
+		latestMessage = null;
+	});
+}
+
+async function waitForLatestMessageToInitialize() {
+	console.log('Wait for latest message to be initialized');
+	while (!latestMessage) await sleep(500);
+	console.log(`Latest message initalized: ${latestMessage}`);
+}
+
+function publishLatestMessage(client) {
+	if (latestMessage) {
+		message = JSON.stringify(latestMessage);
+		console.log(`Publish latest message ${message}`);
+		client.write(`${message}\r\n`); 
+	}
+}
+
+async function onClientConnection(client) {
 	console.log('Client connection established');
+	
 	let stop = false;
 	
 	client.on('end', function() {
@@ -46,25 +69,25 @@ async function publishLatestMessage(client) {
 		stop = true;
 	})
 	
+	fetchLatestMessage(nextRoot);
+	await waitForLatestMessageToInitialize();
+		
+	console.log('Start publishing');
 	while (!stop) {
-		client.write(`${latestMessage}\r\n`); // Todo do this in mam callback
-		await sleep(1000);
+		publishLatestMessage(client);
+		fetchLatestMessage(nextRoot);
+		await sleep(pollingIntervallMilliseconds);
 	}
 }
 
-try {
-	console.log('init mam');
-	Mam.init(provider);
-	console.log('fetch latest message');
-	fetchLatestMessage();
-	// Todo subscribe to mam updates and save them to latest message variable.
-	// Todo send latest message to client when changed
+console.log('Init mam state');
+Mam.init(provider);
 
-	const server = net.createServer(publishLatestMessage);
-	server.listen(listeningPort, localhost, () => { 
-		console.log(`Server listening for connection on port ${listeningPort}`);
-	});
+console.log('Init server');
+const server = net.createServer(onClientConnection);
 
-} catch (error) {
-	console.log(`Unhanldled error: ${error}`);
-}
+server.on('error', function(error) { console.log(`Unhandled error: ${error}`) })
+
+server.listen(listeningPort, localhost, () => { 
+	console.log(`Server listening for connection on port ${listeningPort}`);
+});
