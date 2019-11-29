5dffbb7e95699e43bd53de80b6c273b80bc47d1e
Ivar Contessotto <ivar.contessotto@stud.hslu.ch>
2019-11-29 19:05:34 +0100

    fix JSONPathHelper

--- src/main/java/oracle/input/provider/JSONPathHelper.java
+++ src/main/java/oracle/input/provider/JSONPathHelper.java
@@ -2,29 +2,41 @@
 package oracle.input.provider;
 
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Queue;
 
 public class JSONPathHelper {
 
+    private static Logger LOGGER = LogManager.getLogger(JSONPathHelper.class);
+
     public static String find(String source, Queue<JsonPath> queries) {
 
-        Object value = null;
-        JsonPath query = queries.poll();
+        try {
 
-        if (query != null) {
-            value = query.read(source);
-            query = queries.poll();
-        }
+            Object value = null;
+            JsonPath query = queries.poll();
 
-        while (value != null && query != null) {
-            value = query.read(value);
-            query = queries.poll();
-        }
+            if (query != null) {
+                value = query.read(source);
+                query = queries.poll();
+            }
 
-        if (value != null) {
-            return String.valueOf(value);
+            while (value != null && query != null) {
+                value = query.read(value);
+                query = queries.poll();
+            }
+
+            if (value != null) {
+                return String.valueOf(value);
+            }
+            return null;
+
+        } catch (PathNotFoundException e) {
+            LOGGER.debug(e.getMessage());
+            return null;
         }
-        return null;
     }
 }
